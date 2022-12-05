1ed31e7735ba377ec1e516b34f2d134a6ecd0a64
duro.codes <59978280+durimf@users.noreply.github.com>
2022-12-05 11:32:27 +0100

    Sets up Storybook

---
+++ prolog-app/stories/HomePage.stories.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import HomePage from "../pages";
+
+export default {
+  title: "Pages/Header",
+  component: HomePage,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "fullscreen",
+  },
+} as ComponentMeta<typeof HomePage>;
+
+const Template: ComponentStory<typeof HomePage> = (args) => <HomePage />;
+
+export const Default = Template.bind({});
+Default.args = {
+  user: {
+    name: "Jane Doe",
+  },
+};
